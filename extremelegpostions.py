9fca62c643040497af56cbfd6e016cc9bafcab3e
ginoekzhang <159699563+ginoekzhang@users.noreply.github.com>
2025-12-09 04:53:32 -0500

    Update extremelegpostions.py

--- extremelegpostions.py
+++ extremelegpostions.py
@@ -31,18 +31,18 @@ except Error as e1:
 t = 0
 
 time.sleep(3)
-servo1.move(35)
-servo2.move(205)
+servo1.move(120)
+servo2.move(120)
 
 time.sleep(3)
-servo3.move(205)
-servo4.move(35)
+servo3.move(120)
+servo4.move(120)
 
 time.sleep(3)
-servo5.move(35)
-servo6.move(205)
+servo5.move(120)
+servo6.move(120)
 
 time.sleep(3)
-servo7.move(205)
-servo8.move(35)
+servo7.move(120)
+servo8.move(120)
 time.sleep(3)
