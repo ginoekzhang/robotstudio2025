2cc163344cdb85e34fdb278b9a88cc3d6faece56
ginoekzhang <159699563+ginoekzhang@users.noreply.github.com>
2025-12-09 05:56:38 -0500

    Update extremelegpostions.py

--- extremelegpostions.py
+++ extremelegpostions.py
@@ -22,15 +22,6 @@ try:
     servo7.set_angle_limits(0, 240)
     servo8.set_angle_limits(0, 240)
 
-        
-    servo3.set_angle_offset(+10, permanent=True)
-    servo4.set_angle_offset(-15, permanent=True)
-    
-    servo5.set_angle_offset(-25, permanent=True)
-    servo6.set_angle_offset(-20, permanent=True)
-    
-    servo7.set_angle_offset(-25, permanent=True)
-    servo8.set_angle_offset(-15, permanent=True)
 
 except ServoTimeoutError as e:
     print(f"Servo {e.id_} is not responding. Exiting...")
@@ -41,19 +32,29 @@ except Error as e1:
 
 t = 0
 
-time.sleep(3)
-servo1.move(120)
-servo2.move(120)
+#servo3.set_angle_offset(+10, permanent=True)
+#servo4.set_angle_offset(-15, permanent=True)
+
+#servo5.set_angle_offset(-25, permanent=True)
+#servo6.set_angle_offset(-20, permanent=True)
+
+#servo7.set_angle_offset(-25, permanent=True)
+#servo8.set_angle_offset(-15, permanent=True)
+
 
 time.sleep(3)
-servo3.move(120)
-servo4.move(120)
+servo1.move(35)
+servo2.move(205)
 
 time.sleep(3)
-servo5.move(120)
-servo6.move(120)
+servo3.move(45)
+servo4.move(190)
 
 time.sleep(3)
-servo7.move(120)
-servo8.move(120)
+servo5.move(180)
+servo6.move(15)
+
+time.sleep(3)
+servo7.move(180)
+servo8.move(20)
 time.sleep(3)
