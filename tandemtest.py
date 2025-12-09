43b3de0deaa169ca292ca1d1f5715898c421cd4e
ginoekzhang <159699563+ginoekzhang@users.noreply.github.com>
2025-12-09 04:02:35 -0500

    Create tandemtest.py

---
+++ tandemtest.py
@@ -0,0 +1,47 @@
+from math import sin, cos, pi
+from pylx16a.lx16a import *
+import time
+
+LX16A.initialize("/dev/ttyUSB0")
+
+try:
+    servo1 = LX16A(1)
+    servo2 = LX16A(2)
+    servo3 = LX16A(3)
+    servo4 = LX16A(4)
+    servo1.set_angle_limits(0, 240)
+    servo2.set_angle_limits(0, 240)
+    servo3.set_angle_limits(0, 240)
+    servo4.set_angle_limits(0, 240)
+except ServoTimeoutError as e:
+    print(f"Servo {e.id_} is not responding. Exiting...")
+    quit()
+except Error as e1:
+    print(f"Unexpected Error with servo {e.id_}. Exiting...")
+    quit()
+
+t = 0
+
+time.sleep(3)
+servo1.move(75)
+servo2.move(205)
+servo3.move(165)
+servo4.move(35)
+time.sleep(3)
+
+while True:
+    servo2.move(35)
+    servo1.move(165)
+    servo4.move(205)
+    servo3.move(75)
+    
+    time.sleep(1)
+    servo1.move(75)
+    servo2.move(205)
+    servo3.move(165)
+    servo4.move(35)
+    time.sleep(1)
+
+
+#    time.sleep(3)
+    #t += 0.1
