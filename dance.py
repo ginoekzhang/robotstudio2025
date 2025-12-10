009870fe8117968333e6b9c04ef59356382632dd
ginoekzhang <159699563+ginoekzhang@users.noreply.github.com>
2025-12-09 19:10:28 -0500

    Create dance.py

---
+++ dance.py
@@ -0,0 +1,60 @@
+from math import sin, cos, pi
+from pylx16a.lx16a import *
+import time
+
+LX16A.initialize("/dev/ttyUSB0")
+
+try:
+    servo1 = LX16A(1)
+    servo2 = LX16A(2)
+    servo3 = LX16A(3)
+    servo4 = LX16A(4)
+    servo5 = LX16A(5)
+    servo6 = LX16A(6)
+    servo7 = LX16A(7)
+    servo8 = LX16A(8)
+    servo1.set_angle_limits(0, 240)
+    servo2.set_angle_limits(0, 240)
+    servo3.set_angle_limits(0, 240)
+    servo4.set_angle_limits(0, 240)
+    servo5.set_angle_limits(0, 240)
+    servo6.set_angle_limits(0, 240)
+    servo7.set_angle_limits(0, 240)
+    servo8.set_angle_limits(0, 240)
+
+
+except ServoTimeoutError as e:
+    print(f"Servo {e.id_} is not responding. Exiting...")
+    quit()
+except Error as e1:
+    print(f"Unexpected Error with servo {e.id_}. Exiting...")
+    quit()
+
+t = 1000
+
+#OFFSETS = [0, 0, 10, -15, -30, -15, -25, -15]
+
+time.sleep(1)
+servo1.move(140)
+servo2.move(85)
+
+servo3.move(110)
+servo4.move(135)
+
+servo5.move(70)
+servo6.move(135)
+
+servo7.move(115)
+servo8.move(70)
+time.sleep(3)
+
+while True:
+  servo5.move(60, t)
+  servo6.move(125, t)
+  time.sleep(1)
+
+  servo7.move(105, t)
+  servo8.move(60, t)
+  time.sleep(1)
+
+  
